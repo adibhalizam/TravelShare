24e679a7a02c6c0f19c5f37720cad2e5bc92139f
Muhammad Adib Bin Halizam <adibhalizam@gmail.com>
2024-04-15 10:39:18 -0400

    Add favorite and fix navigation

--- models/itineraryModel.js
+++ models/itineraryModel.js
@@ -24,10 +24,6 @@ const itinerarySchema = new mongoose.Schema({
         user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         text: { type: String, required: true },
         createdAt: { type: Date, default: Date.now }
-    }],
-    likes: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
     }]
 }, {
     timestamps: true
